97cb709e6facbc9bb8d758d4fde92e79d6dc5ca6
Pato Vargas <pevargas2730@gmail.com>
2019-11-13 01:14:30 -0500

    rewgistration page

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder } from "@angular/forms";
+import { Validators } from "@angular/forms";
 @Component({
-  selector: 'app-registration',
-  templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+  selector: "app-registration",
+  templateUrl: "./registration.component.html",
+  styleUrls: ["./registration.component.scss"]
 })
 export class RegistrationComponent implements OnInit {
+  registrationForm = this.fb.group({
+    firstName: ["", Validators.required],
+    lastName: ["", Validators.required]
+  });
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onSubmit() {
+    console.log(this.registrationForm.value);
   }
-
 }
