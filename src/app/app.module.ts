965278938eb89b7e354e8ff4d95ca3b574f9ebd4
Pato Vargas <pevargas2730@gmail.com>
2019-11-13 00:08:50 -0500

    created scanner component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { ScannerComponent } from './scanner/scanner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScannerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/scanner/scanner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scanner',
+  templateUrl: './scanner.component.html',
+  styleUrls: ['./scanner.component.scss']
+})
+export class ScannerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
