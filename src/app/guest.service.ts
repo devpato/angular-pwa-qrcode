20e324ae43d01afce02bc92b889302d4e3838e50
Pato Vargas <pevargas2730@gmail.com>
2019-11-14 00:01:00 -0500

    behavior subject guest service

--- src/app/guest.service.ts
+++ src/app/guest.service.ts
@@ -1,9 +1,34 @@
-import { Injectable } from '@angular/core';
-
+import { Injectable } from "@angular/core";
+import { Guest } from "./guest.model";
+import { v4 as uuid } from "uuid";
+import { BehaviorSubject } from "rxjs";
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class GuestService {
+  private guestSource = new BehaviorSubject<Guest[]>(null);
+  guests$ = this.guestSource.asObservable();
+  private guests = [
+    {
+      id: uuid(),
+      firstName: "Pato",
+      lastName: "Vargas",
+      qr: "asdfadfadsfadsfsa"
+    },
+    {
+      id: uuid(),
+      firstName: "Diego",
+      lastName: "Maradona",
+      qr: "asdfadfadsfadsfsa"
+    }
+  ];
 
-  constructor() { }
+  constructor() {
+    this.guestSource.next(this.guests);
+  }
+
+  addGuest(newGuest: Guest): void {
+    this.guests.push(newGuest);
+    this.guestSource.next(this.guests);
+  }
 }
