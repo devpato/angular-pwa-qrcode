7ea7f5bccfa1b5cf73b87add3923f1f6c7523380
Pato Vargas <pevargas2730@gmail.com>
2019-11-14 00:00:26 -0500

    added more routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { RegistrationComponent } from "./registration/registration.component";
+import { GuestListComponent } from "./guest-list/guest-list.component";
+import { ScannerComponent } from "./scanner/scanner.component";
 
 const routes: Routes = [
   {
     path: "",
     component: RegistrationComponent
+  },
+  {
+    path: "guest",
+    component: GuestListComponent
+  },
+  {
+    path: "scanner",
+    component: ScannerComponent
   }
 ];
 
