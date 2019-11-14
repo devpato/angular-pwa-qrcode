68b8552eb9f14f904693cb52e184ad8f5431ce40
Pato Vargas <pevargas2730@gmail.com>
2019-11-14 01:08:04 -0500

    added basic code for scanner

--- src/app/scanner/scanner.component.ts
+++ src/app/scanner/scanner.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { Guest } from "../guest.model";
 
 @Component({
-  selector: 'app-scanner',
-  templateUrl: './scanner.component.html',
-  styleUrls: ['./scanner.component.scss']
+  selector: "app-scanner",
+  templateUrl: "./scanner.component.html",
+  styleUrls: ["./scanner.component.scss"]
 })
 export class ScannerComponent implements OnInit {
+  availableDevices: MediaDeviceInfo[];
+  currentDevice: MediaDeviceInfo = null;
+  hasDevices: boolean;
+  hasPermission: boolean;
+  qrResultString: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit(): void {}
+
+  clearResult(): void {
+    this.qrResultString = null;
   }
 
+  onCamerasFound(devices: MediaDeviceInfo[]): void {
+    this.availableDevices = devices;
+    this.hasDevices = Boolean(devices && devices.length);
+  }
+
+  onCodeResult(resultString: string) {
+    this.qrResultString = resultString;
+  }
+
+  onDeviceSelectChange(selected: string) {
+    const device = this.availableDevices.find(x => x.deviceId === selected);
+    this.currentDevice = device || null;
+  }
+
+  onHasPermission(has: boolean) {
+    this.hasPermission = has;
+  }
 }
