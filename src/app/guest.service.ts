24b3e851782b0be2769745b842652bc4a845911c
Pato Vargas <pevargas2730@gmail.com>
2019-11-14 02:21:28 -0500

    populated qr info

--- src/app/guest.service.ts
+++ src/app/guest.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from "@angular/core";
 import { Guest } from "./guest.model";
-import { v4 as uuid } from "uuid";
 import { BehaviorSubject } from "rxjs";
 @Injectable({
   providedIn: "root"
@@ -10,21 +9,24 @@ export class GuestService {
   guests$ = this.guestSource.asObservable();
   private guests = [
     {
-      id: uuid(),
+      id: "7558e6e5-3cfa-4c24-b5b7-653ecbd49925",
       firstName: "Pato",
-      lastName: "Vargas",
-      qr: "asdfadfadsfadsfsa"
+      lastName: "Vargas"
     },
     {
-      id: uuid(),
+      id: "4847498c-b57f-4ceb-8c0c-8831b9972158",
       firstName: "Diego",
-      lastName: "Maradona",
-      qr: "asdfadfadsfadsfsa"
+      lastName: "Maradona"
     }
   ];
 
   constructor() {
+    this.populateQR();
     this.guestSource.next(this.guests);
+  }
+
+  populateQR(): void {
+    this.guests.forEach((g: Guest) => (g.qr = JSON.stringify({ ...g })));
   }
 
   addGuest(newGuest: Guest): void {
