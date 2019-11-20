34ce54db96ad0169ce5b1809d58066c6844a1a9c
Pato Vargas <pevargas2730@gmail.com>
2019-11-19 23:06:51 -0500

    code cleanup

--- src/app/scanner/scanner.component.ts
+++ src/app/scanner/scanner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Guest } from "../guest.model";
 import { GuestService } from "../guest.service";
 import { map } from "rxjs/operators";
